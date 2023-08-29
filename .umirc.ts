79454c619519158d72b7edc866c4af186745d6e4
Min_Liu <Min_Liu@singaporeair.com.sg>
2023-08-29 10:21:45 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -8,14 +8,4 @@ export default {
   history: { type: 'hash' },
   favicons: ['/favicon/4.svg', '/favicon/4.ico'],
   esbuildMinifyIIFE: true,
-
-  pwa: {
-    src: 'manifest.json',
-    manifest: {
-      name: 'PWA',
-    },
-    hash: true,
-    appStatusBar: '#fff',
-  },
-  plugins: [['umijs-plugin-pwa']],
 };
