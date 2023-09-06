e167e6b44bbd84c55ac054de74e04738a74e9af1
Min_Liu <Min_Liu@singaporeair.com.sg>
2023-09-06 08:59:41 +0800

    update: home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,17 +2,9 @@ import * as React from 'react';
 import { history } from 'umi';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
-import Tabs from '@/components/Tabs';
-import CarbinClassSelect from '@/components/CarbinClassSelect';
-import FlightCard from '@/components/FlightCard';
-import MarketingAirlineCodeSQIcon from '@/components/FlightCard/sq.svg';
-import MarketingAirlineCodeScootIcon from '@/components/FlightCard/scoot.svg';
 import DestinationCard from './components/DesctinationCard';
 
 const list = [
